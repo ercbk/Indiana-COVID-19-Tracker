cadb8a81930652745acc900e60423c42c4039963
ercbk <ericbookax0@yahoo.com>
2020-04-11 10:49:23 -0400

    add create_token script for github actions and set env vars in workflow

---
+++ automate/create_token.R
@@ -0,0 +1,12 @@
+# Create rtweet token to use in github actions
+
+
+
+token_stuff <- Sys.getenv(c("APPNAME", "APIKEY", "APISECRET", "ACCESSTOKEN", "ACCESSSECRET"))
+
+create_token(
+      app = token_stuff[[1]],
+      consumer_key = token_stuff[[2]],
+      consumer_secret = token_stuff[[3]],
+      access_token = token_stuff[[4]],
+      access_secret = token_stuff[[5]])
