0f254dd2896b3302f307dcdee6d4daea6f5329bd
ercbk <ericbookax0@yahoo.com>
2020-04-19 20:06:55 -0400

    remove Roboto from text arg of theme in ind-line

--- R/ind-covid-line.R
+++ R/ind-covid-line.R
@@ -158,7 +158,6 @@ ind_statewide_pos <- ggplot(ind_dat, aes(x = date, y = positives)) +
                     color = deep_rooted[[7]]) +
    labs(x = NULL, y = NULL, title = "<b style='color:#B28330'>Cumulative Positive Test Results</b>") +
    theme(plot.title = element_textbox_simple(size = rel(0.9)),
-         text = element_text(family = "Roboto"),
          legend.position = "none",
          axis.text.x = element_text(color = "white"),
          axis.text.y = element_text(color = "white"),
@@ -189,7 +188,6 @@ ind_statewide_dea <- ggplot(ind_dat, aes(x = date, y = deaths)) +
    labs(x = NULL, y = NULL, title = "<b style='color:#BE454F'>Cumulative Deaths</b>") +
    theme(plot.title = element_textbox_simple(size = rel(0.9),
                                              family = "Roboto"),
-         text = element_text(family = "Roboto"),
          legend.position = "none",
          axis.text.x = element_text(color = "white"),
          axis.text.y = element_text(color = "white"),
