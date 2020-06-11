62c2a049c82008064b4365960ac350eae7bfe68d
ercbk <ericbookax0@yahoo.com>
2020-06-10 21:10:42 -0400

    sel-test edit

--- R/sel-test.R
+++ R/sel-test.R
@@ -2,39 +2,44 @@ library(RSelenium)
 
 # Use RSelenium to download dataset
 # start selenium server
-driver <- rsDriver(browser = c("chrome"), chromever = "81.0.4044.138")
-Sys.sleep(10)
 
-# browser
-chrome <- driver$client
-# currently this isn't needed to shutdown selenium server
-#server <- driver$server
+sel <- wdman::selenium()
+sel$log()
 
-url <- "https://www.opentable.com/state-of-industry"
-# go to website
-chrome$navigate(url = url)
 
-# css selector for the data download button
-dl_button <- chrome$findElement(using = "css",
-                                value = "#content > div > div > main > section:nth-child(2) > div:nth-child(4) > div._3ZR5BNaRxlZSImxhkkEzrb > button > div")
-
-# makes the element flash in the browser so you can confirm you have the right thing
-# dl_button$highlightElement()
-
-dl_button$clickElement()
-# give it a few secs to d/l
-Sys.sleep(5)
-
-filename <- "YoY_Seated_Diner_Data.csv"
-download_location <- file.path(Sys.getenv("USERPROFILE"), "Downloads")
-# moves file from download folder to data folder in my project directory
-file.rename(file.path(download_location, filename), glue("{rprojroot::find_rstudio_root_file()}/data/{filename}"))
-
-# close browser
-chrome$close()
-
-# currently this doesn't shutdown the server
-# server$stop()
-
-# kill the server manually
-installr::kill_process(process = "java.exe")
+# driver <- rsDriver(browser = c("chrome"), chromever = "81.0.4044.138")
+# Sys.sleep(10)
+# 
+# # browser
+# chrome <- driver$client
+# # currently this isn't needed to shutdown selenium server
+# #server <- driver$server
+# 
+# url <- "https://www.opentable.com/state-of-industry"
+# # go to website
+# chrome$navigate(url = url)
+# 
+# # css selector for the data download button
+# dl_button <- chrome$findElement(using = "css",
+#                                 value = "#content > div > div > main > section:nth-child(2) > div:nth-child(4) > div._3ZR5BNaRxlZSImxhkkEzrb > button > div")
+# 
+# # makes the element flash in the browser so you can confirm you have the right thing
+# # dl_button$highlightElement()
+# 
+# dl_button$clickElement()
+# # give it a few secs to d/l
+# Sys.sleep(5)
+# 
+# filename <- "YoY_Seated_Diner_Data.csv"
+# download_location <- file.path(Sys.getenv("USERPROFILE"), "Downloads")
+# # moves file from download folder to data folder in my project directory
+# file.rename(file.path(download_location, filename), glue("{rprojroot::find_rstudio_root_file()}/data/{filename}"))
+# 
+# # close browser
+# chrome$close()
+# 
+# # currently this doesn't shutdown the server
+# # server$stop()
+# 
+# # kill the server manually
+# installr::kill_process(process = "java.exe")
