c45eae8576d3059e283cd6ec591cf355db39c0a5
ercbk <ericbookax0@yahoo.com>
2020-06-21 20:17:50 -0400

    re-run build-datasets

--- R/build-datasets.R
+++ R/build-datasets.R
@@ -15,7 +15,7 @@ library(dplyr, warn.conflicts = F, quietly = T)
 ###########################
 
 
-todays_date <- lubridate::today()
+todays_date <- lubridate::today()-1
 
 try_date_str <- todays_date %>% 
       stringr::str_extract(pattern = "-[0-9]{2}-[0-9]{2}") %>% 
@@ -27,15 +27,15 @@ try_address <- glue::glue("https://hub.mph.in.gov/dataset/5a905d51-eb50-4a83-8f7
 try_destfile <- glue::glue("data/beds-vents-{try_date_str}.xlsx")
 download.file(try_address, destfile = try_destfile, mode = "wb")
 
-bv_dat_current <- readxl::read_xlsx(try_destfile) %>% 
+bv_dat_current <- readxl::read_xlsx(try_destfile) %>%
       tidyr::pivot_wider(names_from = "STATUS_TYPE", values_from = "TOTAL") %>%
-      mutate(date = todays_date) %>% 
+      mutate(date = todays_date) %>%
       select(date, everything())
 
 
 old_complete <- readr::read_rds("data/beds-vents-complete.rds")
 
-new_complete <- old_complete %>% 
+new_complete <- old_complete %>%
       bind_rows(bv_dat_current)
 
 readr::write_rds(new_complete, "data/beds-vents-complete.rds")
@@ -43,10 +43,10 @@ readr::write_csv(new_complete, "data/beds-vents-complete.csv")
 
 
 # keep a week's worth of files, delete anything older
-delete_date <- todays_date - 7 
-del_date_str <- delete_date %>% 
-   stringr::str_extract(pattern = "-[0-9]{2}-[0-9]{2}") %>% 
-   stringr::str_remove_all(pattern = "-") %>% 
+delete_date <- todays_date - 7
+del_date_str <- delete_date %>%
+   stringr::str_extract(pattern = "-[0-9]{2}-[0-9]{2}") %>%
+   stringr::str_remove_all(pattern = "-") %>%
    stringr::str_remove(pattern = "^[0-9]")
 
 fs::file_delete(glue::glue("{rprojroot::find_rstudio_root_file()}/data/beds-vents-{del_date_str}.xlsx"))
