2ebed942acdc5bbb68f0a0bc336e66a8323d51b2
ercbk <ericbookax0@yahoo.com>
2020-09-08 17:57:24 -0400

    update - revert cron time, detect - change hour condition from 17 to 5

--- automate/detect-data-update.R
+++ automate/detect-data-update.R
@@ -22,8 +22,8 @@ rt_tok <- rtweet::create_token(
 # need to set the initial value
 tweet_rows <- 0
 
-# Runner uses UTC timezone so 17:00 should be 1pm ET
-while (lubridate::hour(Sys.time()) <= 17 & tweet_rows == 0) {
+# Runner uses UTC timezone so 5:00 should be 1pm ET
+while (lubridate::hour(Sys.time()) <= 5 & tweet_rows == 0) {
    
    # detect pattern in tweet that has updated data
    in_health_tweets <- rtweet::get_timeline("StateHealthIN",
