071ab1486641f49536adf8c5b8b3a2d0999c9ebe
Eric-Laptop <nullspaceax0@gmail.com>
2020-11-30 18:54:19 -0500

    build-dat - ctp changed a url, so ran manually

--- R/build-datasets.R
+++ R/build-datasets.R
@@ -78,7 +78,7 @@ fs::file_delete(glue::glue("{rprojroot::find_rstudio_root_file()}/data/beds-vent
 
 
 # The Covid Tracking Project
-race_dat_raw <- readr::read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vR_xmYt4ACPDZCDJcY12kCiMiH0ODyx3E1ZvgOHB8ae1tRcjXbs_yWBOA4j4uoCEADVfC1PS2jYO68B/pub?gid=43720681&single=true&output=csv")
+race_dat_raw <- readr::read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vS8SzaERcKJOD_EzrtCDK1dX1zkoMochlA9iHoHg_RSw3V8bkpfk1mpw4pfL5RdtSOyx_oScsUtyXyk/pub?gid=43720681&single=true&output=csv")
 
 race_date <- race_dat_raw %>% 
    janitor::clean_names() %>% 
