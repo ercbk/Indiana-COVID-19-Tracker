8e350b5f0d5088f0ad13d322412171445fb11723
ercbk <ericbookax0@yahoo.com>
2021-04-27 21:23:01 -0400

    pos-policy: changed policy annotation, changed age group vax shape

--- R/static-charts/pos-policy-line.R
+++ R/static-charts/pos-policy-line.R
@@ -21,7 +21,7 @@
 
 
 
-pacman::p_load(extrafont, swatches, dplyr, tsibble, ggplot2, ggtext, glue, ggstar, ggrepel)
+pacman::p_load(extrafont, swatches, dplyr, tsibble, ggplot2, ggtext, glue, ggrepel)
 
 nyt_dat <- readr::read_csv("https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv")
 
@@ -43,52 +43,52 @@ options(scipen = 999)
 
 # calculated daily positive cases
 cases_dat <- nyt_dat %>% 
-   filter(state == "Indiana") %>% 
-   mutate(daily_cases = difference(cases),
-          daily_cases = tidyr::replace_na(daily_cases, 1),
-          sev_day_avg = slider::slide_dbl(daily_cases, .f = mean, .before = 6L),
-          # for facet_zoom
-          fall_wave = ifelse(date > as.Date("2020-09-26"), "fwave", "not_fwave")) %>%
-   filter(date >= "2020-04-20") %>% 
-   rename(cumulative_cases = cases)
+      filter(state == "Indiana") %>% 
+      mutate(daily_cases = difference(cases),
+             daily_cases = tidyr::replace_na(daily_cases, 1),
+             sev_day_avg = slider::slide_dbl(daily_cases, .f = mean, .before = 6L),
+             # for facet_zoom
+             fall_wave = ifelse(date > as.Date("2020-09-26"), "fwave", "not_fwave")) %>%
+      filter(date >= "2020-04-20") %>% 
+      rename(cumulative_cases = cases)
 
 # current date of data
 data_date <- cases_dat %>%
-   summarize(date = max(date)) %>% 
-   pull(date)
+      summarize(date = max(date)) %>% 
+      pull(date)
 
 policy_dat <- tibble(policy = "Stage 2",
                      date = as.Date("2020-05-04"),
                      date_text = "5/04/2020") %>% 
-   add_row(policy = "Stage 3",
-           date = as.Date("2020-05-22"),
-           date_text = "5/22/2020") %>% 
-   add_row(policy = "Stage 4",
-           date = as.Date("2020-06-12"),
-           date_text = "6/12/2020") %>% 
-   add_row(policy = "Stage 4.5",
-           date = as.Date("2020-07-03"),
-           date_text = "7/3/2020") %>% 
-   add_row(policy = "Conditional Mask Requirement",
-           date = as.Date("2020-07-27"),
-           date_text = "7/27/2020") %>% 
-   add_row(policy = "Stage 5",
-           date = as.Date("2020-09-26"),
-           date_text = "9/26/2020") %>% 
-   add_row(policy = "County-score Gathering Restrictions",
-           date = as.Date("2020-11-14"),
-           date_text = "11/14/2020") %>%
-   add_row(policy = "Mask Requirement Ends",
-           date = as.Date("2021-04-06"),
-           date_text = "04/06/2021") %>% 
-   mutate(labels = c("2", "3", "4", "4.5", "CMR", "5", "CGR", "Mask Requirement Ends"))
+      add_row(policy = "Stage 3",
+              date = as.Date("2020-05-22"),
+              date_text = "5/22/2020") %>% 
+      add_row(policy = "Stage 4",
+              date = as.Date("2020-06-12"),
+              date_text = "6/12/2020") %>% 
+      add_row(policy = "Stage 4.5",
+              date = as.Date("2020-07-03"),
+              date_text = "7/3/2020") %>% 
+      add_row(policy = "Conditional Mask Requirement",
+              date = as.Date("2020-07-27"),
+              date_text = "7/27/2020") %>% 
+      add_row(policy = "Stage 5",
+              date = as.Date("2020-09-26"),
+              date_text = "9/26/2020") %>% 
+      add_row(policy = "County-score Gathering Restrictions",
+              date = as.Date("2020-11-14"),
+              date_text = "11/14/2020") %>%
+      add_row(policy = "Mask Requirement Ends",
+              date = as.Date("2021-04-06"),
+              date_text = "04/06/2021") %>% 
+      mutate(labels = c("2", "3", "4", "4.5", "CMR", "5", "CGR", "Mask Requirement Ends"))
 
 holiday_dat <- tibble(holiday = c("Memorial Day", "Independence Day", "Labor Day", "Thanksgiving",
                                   "Christmas", "New Years Eve", "Super Bowl", "Easter"),
                       date = as.Date(c("2020-05-25", "2020-07-04", "2020-09-07", "2020-11-26",
                                        "2020-12-25", "2020-12-31", "2021-02-07", "2021-04-04"))) %>% 
-   inner_join(cases_dat %>% 
-                 select(date, cumulative_cases, daily_cases), by = "date")
+      inner_join(cases_dat %>% 
+                       select(date, cumulative_cases, daily_cases), by = "date")
 
 vax_dat <- tibble(age = c("80+", "70+", "65-69", "60-64",
                           "55-59", "50-54", "45-49",
@@ -98,8 +98,8 @@ vax_dat <- tibble(age = c("80+", "70+", "65-69", "60-64",
                                    "2021-03-16", "2021-03-22", "2021-03-29", "2021-03-31")),
                   labels = c("80", "70", "65", "60", "55",
                              "50", "45", "40", "30", "16")) %>% 
-   inner_join(cases_dat %>% 
-                 select(date, cumulative_cases, daily_cases), by = "date")
+      inner_join(cases_dat %>% 
+                       select(date, cumulative_cases, daily_cases), by = "date")
 
 
 #@@@@@@@@@@@@@@@@@@@@@@@@@@@@
@@ -109,23 +109,23 @@ vax_dat <- tibble(age = c("80+", "70+", "65-69", "60-64",
 
 # calc difference between one day and the previous day
 daily_change <- cases_dat %>%
-   mutate(cases_diff = difference(daily_cases)) %>%
-   arrange(desc(date)) %>% 
-   pull(cases_diff)
+      mutate(cases_diff = difference(daily_cases)) %>%
+      arrange(desc(date)) %>% 
+      pull(cases_diff)
 
 # calculate how many consecutive days of increasing/decreasing daily cases
 count_consec_days <- function(x) {
-   # rle: "run length encoding," counts runs of same value or in this case, the same sign
-   pos_runs <- rle(sign(x))
-   conseq_days <- tibble(
-      num_days = pos_runs$lengths,
-      sign = pos_runs$values
-   ) %>% 
-      mutate(trend = case_when(sign == 1 ~ "increased",
-                               sign == -1 ~ "decreased",
-                               TRUE ~ "no change in")) %>% 
-      slice(1) %>% 
-      select(-sign)
+      # rle: "run length encoding," counts runs of same value or in this case, the same sign
+      pos_runs <- rle(sign(x))
+      conseq_days <- tibble(
+            num_days = pos_runs$lengths,
+            sign = pos_runs$values
+      ) %>% 
+            mutate(trend = case_when(sign == 1 ~ "increased",
+                                     sign == -1 ~ "decreased",
+                                     TRUE ~ "no change in")) %>% 
+            slice(1) %>% 
+            select(-sign)
 }
 
 consec_days <- count_consec_days(daily_change)
@@ -142,17 +142,17 @@ under_neg_one <- glue("<b style='color: #33a532'>{consec_days$num_days[[1]]}</b>
 
 # choose the subtitle text based number of consecutive days and trend
 subtitle_dat <- consec_days %>% 
-   mutate(text = case_when(num_days == 1 & trend == "increased" ~
-                              pos_one,
-                           num_days == 1 & trend == "decreased" ~
-                              neg_one,
-                           between(num_days, 2, 4) & trend == "increased" ~
-                              under_five,
-                           num_days >= 5 & trend == "increased" ~
-                              five_over,
-                           num_days > 1 & trend == "decreased" ~
-                              under_neg_one,
-                           TRUE ~ zero_days))
+      mutate(text = case_when(num_days == 1 & trend == "increased" ~
+                                    pos_one,
+                              num_days == 1 & trend == "decreased" ~
+                                    neg_one,
+                              between(num_days, 2, 4) & trend == "increased" ~
+                                    under_five,
+                              num_days >= 5 & trend == "increased" ~
+                                    five_over,
+                              num_days > 1 & trend == "decreased" ~
+                                    under_neg_one,
+                              TRUE ~ zero_days))
 
 
 
@@ -163,10 +163,10 @@ subtitle_dat <- consec_days %>%
 
 
 label_dat <- cases_dat %>%
-   as_tibble() %>%
-   # inner_join only keeps dates with a policy associated with it
-   inner_join(policy_dat, by = "date") %>%
-   select(-deaths, -fips, -state)
+      as_tibble() %>%
+      # inner_join only keeps dates with a policy associated with it
+      inner_join(policy_dat, by = "date") %>%
+      select(-deaths, -fips, -state)
 
 
 # arrow specification used below; trying to keep the ggplot mess to a minimum
@@ -177,18 +177,19 @@ caption_text_20 <- glue("Source: The New York Times, based on reports from state
 caption_text_21 <- glue("Last updated: {data_date}")
 
 holiday_text <- "<span style='font-family: \"Font Awesome 5 Free Solid\"; color: #D5AB62FF; font-size:18pt'>&#9830;</span> Holiday"
-vax_text <- "<span style='font-family: \"Font Awesome 5 Free Solid\"; color: #30b278; font-size:18pt'>+</span> Age group becomes eligible for vaccine"
+vax_text <- "<span style='font-family: \"Font Awesome 5 Free Solid\"; color: #30b278; font-size:18pt'>&#9679;</span> Age group becomes eligible for vaccine"
+pol_text <- "<span style='font-family: \"Font Awesome 5 Free Solid\"; color: #306bb2; font-size:18pt'>&#9679;</span> Mask Requirement Ends"
 
 
 
 xmax <- cases_dat %>% 
-   filter(date == max(date)) %>% 
-   mutate(xmax = cumulative_cases * 1.03) %>% 
-   pull(xmax)
+      filter(date == max(date)) %>% 
+      mutate(xmax = cumulative_cases * 1.03) %>% 
+      pull(xmax)
 ymax <- cases_dat %>% 
-   filter(daily_cases == max(daily_cases)) %>% 
-   mutate(ymax = daily_cases * 1.06) %>% 
-   pull(ymax)
+      filter(daily_cases == max(daily_cases)) %>% 
+      mutate(ymax = daily_cases * 1.06) %>% 
+      pull(ymax)
 
 policy_text <- glue("
                     <b style= 'font-size: 16px'>Reopening Stages</b><br><br>
@@ -387,79 +388,63 @@ policy_text <- glue("
 ## 2021 chart ----
 
 pos_policy_one <- ggplot(cases_dat %>% 
-                            as_tibble() %>% 
-                            filter(date > as.Date("2020-12-31")), aes(x = cumulative_cases, y = daily_cases)) +
-   geom_point(color = "#B28330") +
-   geom_point(data = holiday_dat %>% 
-                 filter(date > "2020-12-31"), color = light_orange, shape = 18, size = 4, stroke = 1.5) +
-   geom_star(data = vax_dat, color = "#30b278", starshape = 29,
-             size = 3, starstroke = 1, fill = "#30b278") +
-   geom_line(color = "#B28330") +
-   scale_y_continuous(limits = c(0, ymax), labels = scales::label_comma()) +
-   scale_x_continuous(limits = c(515000, xmax), labels = scales::label_comma()) +
-   geom_text_repel(data = vax_dat, aes(label = labels),
-                   color = "#30b278", fontface = "bold.italic", point.padding = 14,
-                   size = 5, direction = "y", seed = 10) +
-   geom_richtext(data = tibble(x = 600000, y = ymax, zoom = TRUE),
-                 aes(x = x, y = y, label = holiday_text, hjust = "left",
-                     label.color = NA, size = 12, fontface = "bold"),
-                 fill = "black", color = "white") +
-   geom_richtext(data = tibble(x = 600000, y = ymax-500),
-                 aes(x = x, y = y, label = vax_text, hjust = "left",
-                     label.color = NA, size = 12, fontface = "bold"),
-                 fill = "black", color = "white") +
-   geom_text(aes(x = 515000, y = ymax, label="Daily Cases"),
-             family="Roboto",
-             size=4.5, hjust=0.35, color="white") +
-   geom_richtext(data = label_dat %>%
-                    filter(date > "2021-01-01"),
-                 aes(x = cumulative_cases,
-                     y = daily_cases,
-                     label= labels, fontface = "bold.italic",
-                     label.colour = "black",
-                     hjust = "middle", vjust = "center"),
-                 family="Roboto", lineheight=0.95,
-                 size=4.5, label.size=0,
-                 color = "white", fill = "black",
-                 nudge_x = 24000,
-                 nudge_y = -300) +
-   # Mask Requirement Ends
-   geom_curve(
-      data = data.frame(x = 710000, xend = 696000,
-                        y = 100, yend = 300),
-      aes(x = x, xend = xend,
-          y = y, yend = yend),
-      color = deep_light[[7]], arrow = arw,
-      curvature = -0.80
-   ) +
-   labs(x = "Cumulative Cases", y = NULL,
-        title = "2021: Daily <b style='color:#B28330'>Positive Test Results</b> vs. Cumulative <b style='color:#B28330'>Positive Test Results</b>",
-        subtitle = subtitle_dat$text[[1]],
-        caption = caption_text_21) +
-   theme(plot.title = element_textbox_simple(size = 16,
-                                             color = "white",
-                                             family = "Roboto"),
-         plot.subtitle = element_textbox_simple(size = 14,
-                                                color = "white"),
-         plot.caption = element_text(color = "white",
-                                     size = 12),
-         text = element_text(family = "Roboto"),
-         legend.position = "none",
-         axis.text.x = element_text(color = "white",
-                                    size = 12),
-         axis.text.y = element_text(color = "white",
-                                    size = 12),
-         axis.title.x = element_textbox_simple(color = "white",
-                                               size = 13),
-         panel.background = element_rect(fill = "black",
-                                         color = NA),
-         plot.background = element_rect(fill = "black",
-                                        color = NA),
-         panel.border = element_blank(),
-         panel.grid.minor = element_blank(),
-         panel.grid.major = element_line(color = deep_rooted[[7]]))
+                               as_tibble() %>% 
+                               filter(date > as.Date("2020-12-31")), aes(x = cumulative_cases, y = daily_cases)) +
+      geom_point(color = "#B28330") +
+      geom_point(data = holiday_dat %>% 
+                       filter(date > "2020-12-31"), color = light_orange, shape = 18, size = 4, stroke = 1.5) +
+      geom_point(data = vax_dat, color = "#30b278", size = 3) +
+      geom_point(data = tibble(x = 695532, y = 614), aes(x, y),
+                 color = "#306bb2", size = 3) +
+      geom_line(color = "#B28330") +
+      scale_y_continuous(limits = c(0, ymax), labels = scales::label_comma()) +
+      scale_x_continuous(limits = c(515000, xmax), labels = scales::label_comma()) +
+      geom_text_repel(data = vax_dat, aes(label = labels),
+                      color = "#30b278", fontface = "bold.italic", point.padding = 14,
+                      size = 5, direction = "y", seed = 10) +
+      geom_richtext(data = tibble(x = 600000, y = ymax, zoom = TRUE),
+                    aes(x = x, y = y, label = holiday_text, hjust = "left",
+                        label.color = NA, size = 12, fontface = "bold"),
+                    fill = "black", color = "white") +
+      geom_richtext(data = tibble(x = 600000, y = ymax-500),
+                    aes(x = x, y = y, label = vax_text, hjust = "left",
+                        label.color = NA, size = 12, fontface = "bold"),
+                    fill = "black", color = "white") +
+      geom_richtext(data = tibble(x = 600000, y = ymax-1000),
+                    aes(x = x, y = y, label = pol_text, hjust = "left",
+                        label.color = NA, size = 12, fontface = "bold"),
+                    fill = "black", color = "white") +
+      geom_text(aes(x = 515000, y = ymax, label="Daily Cases"),
+                family="Roboto",
+                size=4.5, hjust=0.35, color="white") +
+labs(x = "Cumulative Cases", y = NULL,
+     title = "2021: Daily <b style='color:#B28330'>Positive Test Results</b> vs. Cumulative <b style='color:#B28330'>Positive Test Results</b>",
+     subtitle = subtitle_dat$text[[1]],
+     caption = caption_text_21) +
+      theme(plot.title = element_textbox_simple(size = 16,
+                                                color = "white",
+                                                family = "Roboto"),
+            plot.subtitle = element_textbox_simple(size = 14,
+                                                   color = "white"),
+            plot.caption = element_text(color = "white",
+                                        size = 12),
+            text = element_text(family = "Roboto"),
+            legend.position = "none",
+            axis.text.x = element_text(color = "white",
+                                       size = 12),
+            axis.text.y = element_text(color = "white",
+                                       size = 12),
+            axis.title.x = element_textbox_simple(color = "white",
+                                                  size = 13),
+            panel.background = element_rect(fill = "black",
+                                            color = NA),
+            plot.background = element_rect(fill = "black",
+                                           color = NA),
+            panel.border = element_blank(),
+            panel.grid.minor = element_blank(),
+            panel.grid.major = element_line(color = deep_rooted[[7]]))
 
-
+pos_policy_one
 
 plot_path <- glue("{rprojroot::find_rstudio_root_file()}/plots/pos-policy-one-{data_date}.png")
 
