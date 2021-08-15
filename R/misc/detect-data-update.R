38e25fb05bb14c3e2fbee852866ab54da2496955
ercbk <ericbookax0@yahoo.com>
2021-08-15 10:44:33 -0400

    misc detect update: add special circumstance for sat and sun

--- R/misc/detect-data-update.R
+++ R/misc/detect-data-update.R
@@ -22,30 +22,32 @@ rt_tok <- rtweet::create_token(
 # need to set the initial value
 tweet_rows <- 0
 
-lubridate::hour(Sys.time())
-
-# Runner uses UTC timezone so 4:00 should be 12pm ET
-while (lubridate::hour(Sys.time()) <= 17 & tweet_rows == 0) {
+# If it's Sat or Sun then finish script otherwise detect StateHealthIN tweet before finishing script
+if (!lubridate::wday(Sys.time(), label = TRUE) %in% c("Sun", "Sat")) {
    
-   # detect pattern in tweet that has updated data
-   in_health_tweets <- rtweet::get_timeline("StateHealthIN",
-                                            n = 150,
-                                            token = rt_tok) %>% 
-      tidyr::separate(col = "created_at", into = c("date", "time"), sep = " ") %>% 
-      mutate(date = lubridate::as_date(date),
-             time = hms::as_hms(time)) %>%
-      select(date, text) %>% 
-      filter(stringr::str_detect(text, pattern = "latest #COVID19 case information") | stringr::str_detect(text, pattern = "positive cases")) %>% 
-      filter(date == lubridate::today())
-   
-   # If detection successful, tweet_rows != 0
-   tweet_rows <- nrow(in_health_tweets)
-   
-   # if detection unsuccessful, wait two min before trying again
-   if (tweet_rows == 0) {
+   # Runner uses UTC timezone so 4:00 should be 12pm ET
+   while (lubridate::hour(Sys.time()) <= 17 & tweet_rows == 0) {
+      
+      # detect pattern in tweet that has updated data
+      in_health_tweets <- rtweet::get_timeline("StateHealthIN",
+                                               n = 150,
+                                               token = rt_tok) %>% 
+         tidyr::separate(col = "created_at", into = c("date", "time"), sep = " ") %>% 
+         mutate(date = lubridate::as_date(date),
+                time = hms::as_hms(time)) %>%
+         select(date, text) %>% 
+         filter(stringr::str_detect(text, pattern = "latest #COVID19 case information") | stringr::str_detect(text, pattern = "positive cases")) %>% 
+         filter(date == lubridate::today())
+      
+      # If detection successful, tweet_rows != 0
+      tweet_rows <- nrow(in_health_tweets)
+      
+      # if detection unsuccessful, wait two min before trying again
+      if (tweet_rows == 0) {
          Sys.sleep(120)
+      }
+      
    }
-   
 }
 
 
