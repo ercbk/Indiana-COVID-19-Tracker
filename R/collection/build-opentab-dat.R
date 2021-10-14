014aa82685da7255783b86ee52b03fc084add7d0
ercbk <ericbookax0@yahoo.com>
2021-10-14 15:43:32 -0400

    build-opentab: added chrome version detection

--- R/collection/build-opentab-dat.R
+++ R/collection/build-opentab-dat.R
@@ -30,9 +30,51 @@ chrome_pid <- windows_tasks %>%
 tools::pskill(pid = java_pid)
 tools::pskill(pid = chrome_pid)
 
-# Use RSelenium to download dataset
-# start selenium server; chrome version is the version of the latest stable ChromeDriver 
-driver <- rsDriver(browser = c("chrome"), chromever = "92.0.4515.107")
+
+
+
+
+# Get installed stable Google Chrome version ...
+if (xfun::is_unix()) {
+      
+      chrome_driver_version <-
+            system2(command = ifelse(xfun::is_macos(),
+                                     "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
+                                     "google-chrome-stable"),
+                    args = "--version",
+                    stdout = TRUE,
+                    stderr = TRUE) %>%
+            stringr::str_extract(pattern = "(?<=Chrome )(\\d+\\.){3}")
+      
+      ## on Windows a plattform-specific bug prevents us from calling the Google Chrome binary directly to get its version number
+      ## cf. https://bugs.chromium.org/p/chromium/issues/detail?id=158372
+} else if (xfun::is_windows()) {
+      
+      chrome_driver_version <-
+            system2(command = "wmic",
+                    args = 'datafile where name="C:\\\\Program Files (x86)\\\\Google\\\\Chrome\\\\Application\\\\chrome.exe" get Version /value',
+                    stdout = TRUE,
+                    stderr = TRUE) %>%
+            stringr::str_extract(pattern = "(?<=Version=)(\\d+\\.){3}")
+      
+} else rlang::abort(message = "Your OS couldn't be determined (Linux, macOS, Windows) or is not supported.")
+
+# ... and determine most recent ChromeDriver version matching it
+chrome_driver_version %<>%
+      magrittr::extract(!is.na(.)) %>%
+      stringr::str_replace_all(pattern = "\\.",
+                               replacement = "\\\\.") %>%
+      paste0("^",  .) %>%
+      stringr::str_subset(string =
+                                binman::list_versions(appname = "chromedriver") %>%
+                                dplyr::last()) %>%
+      as.numeric_version() %>%
+      max() %>%
+      as.character()
+
+
+driver <- rsDriver(browser = c("chrome"), chromever = chrome_driver_version)
+
 Sys.sleep(10)
 
 # browser
